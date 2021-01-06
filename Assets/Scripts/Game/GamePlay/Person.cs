e23855113153d984d6173b8e776d023e65181a50
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2021-01-05 23:46:20 -0500

    Fixed Person

--- Assets/Scripts/Game/GamePlay/Person.cs
+++ Assets/Scripts/Game/GamePlay/Person.cs
@@ -33,65 +33,65 @@ public class Person : MonoBehaviour
     /// <summary>
     /// the region for people to move within
     /// </summary>
-    private RectTransform mSpace;
+    private RectTransform _space;
     /// <summary>
     /// the health status of person
     /// </summary>
-    private E_Person_Status mStatus = E_Person_Status.Null;
+    private E_Person_Status _status = E_Person_Status.Null;
     /// <summary>
     /// the image of person
     /// </summary>
-    private Image mImg;
+    private Image _img;
     /// <summary>
     /// the city object
     /// </summary>
-    private City mCity;
+    private City _city;
     /// <summary>
     /// if the game has started
     /// </summary>
-    private bool gameStart = false;
+    private bool _gameStart = false;
     #endregion
 
     #region fields of person's moving direction
     /// <summary>
     /// the number of frame before each direction change
     /// </summary>
-    private int changeDirFrame = 100;
+    private int _changeDirFrame = 100;
 
     /// <summary>
     /// current frame number
     /// </summary>
-    private int currentFrame = 0;
+    private float _currentFrame = 0;
 
     /// <summary>
     /// the moving direction of person
     /// </summary>
-    private Vector3 direction = Vector3.zero;
+    private Vector3 _direction = Vector3.zero;
 
     /// <summary>
     /// the moving speed of the person
     /// </summary>
-    private float speed = 0;
+    private float _speed = 0;
     #endregion
 
     #region Fields of person's infection logic
     /// <summary>
     /// the number of frame before each infection check
     /// </summary>
-    private int checkInfectionFrame = 15;
+    private int _checkInfectionFrame = 30;
 
     /// <summary>
     /// current frame number
     /// </summary>
-    private int currentInfectionFrame = 0;
+    private int _currentInfectionFrame = 0;
     /// <summary>
     /// the number of frame before a infected person get into hospital
     /// </summary>
-    private float waitTimeToHospital = 0;
+    private float _waitTimeToHospital = 0;
     #endregion
 
     #region Fields of person's incubation period
-    private float waitTimeToInfected = 0f;
+    private float _waitTimeToInfected = 0f;
     #endregion
 
     //methods
@@ -105,30 +105,30 @@ public class Person : MonoBehaviour
         gameObject.transform.parent = space.gameObject.transform;
         gameObject.transform.localScale = Vector3.one;
         gameObject.transform.localPosition = Vector3.zero;
-        changeDirFrame = Random.Range(100, 200);
-        currentFrame = changeDirFrame + 1;
-        speed = Random.Range(10f, 15f);
-        mCity = city;
+        _changeDirFrame = Random.Range(100, 200);
+        _currentFrame = _changeDirFrame + 1;
+        _speed = Random.Range(20f, 30f);
+        _city = city;
 
-        mSpace = space;
-        mImg = GetComponent<Image>();
+        _space = space;
+        _img = GetComponent<Image>();
 
         if(space.name == "cityRange")
             ChangeStatus(E_Person_Status.Healthy);
         else
             ChangeStatus(E_Person_Status.Hospital);
-        gameObject.transform.localPosition = new Vector3(mSpace.rect.center.x - mSpace.rect.width/2 + Random.Range(0, mSpace.rect.width),
-                                                         mSpace.rect.center.y - mSpace.rect.height/2 + Random.Range(0, mSpace.rect.height),
+        gameObject.transform.localPosition = new Vector3(_space.rect.center.x - _space.rect.width/2 + Random.Range(0, _space.rect.width),
+                                                         _space.rect.center.y - _space.rect.height/2 + Random.Range(0, _space.rect.height),
                                                          0);
 
         EventCenter.GetInstance().AddEventListener("Game Start", () =>
         {
-            gameStart = true;
+            _gameStart = true;
         });
 
         EventCenter.GetInstance().AddEventListener("Game Stop", () =>
         {
-            gameStart = false;
+            _gameStart = false;
         });
     }
     #endregion
@@ -141,22 +141,22 @@ public class Person : MonoBehaviour
     /// <param name="status"></param>
     public void ChangeStatus(E_Person_Status status)
     {
-        mStatus = status;
+        _status = status;
         switch (status)
         {
             case E_Person_Status.Healthy:
-                mImg.color = Color.white;
+                _img.color = Color.white;
                 break;
             case E_Person_Status.Incubation:
-                waitTimeToInfected = 0;
-                mImg.color = Color.yellow;
+                _waitTimeToInfected = 0;
+                _img.color = Color.yellow;
                 break;
             case E_Person_Status.Infected:
-                waitTimeToHospital = 0;
-                mImg.color = Color.red;
+                _waitTimeToHospital = 0;
+                _img.color = Color.red;
                 break;
             case E_Person_Status.Hospital:
-                mImg.color = Color.blue;
+                _img.color = Color.blue;
                 break;
         }
     }
@@ -169,45 +169,46 @@ public class Person : MonoBehaviour
     /// </summary>
     private void RandomMove()
     {
-        currentFrame++;
-        if (currentFrame > changeDirFrame)
+        _currentFrame++;
+        if (_currentFrame > _changeDirFrame)
         {
             if (Random.Range(0f, 1f) < Virus.intentionToMove)
             {
-                direction.Set(Random.Range(-50, 50), Random.Range(-50, 50), 0);
-                direction.Normalize();
+                _direction.Set(Random.Range(-50, 50), Random.Range(-50, 50), 0);
+                _direction.Normalize();
             }
             else
-                direction = Vector3.zero;
+                _direction = Vector3.zero;
 
-            currentFrame = 0;
+            _currentFrame = 0;
         }
 
-        if (gameObject.transform.localPosition.x < mSpace.rect.center.x - mSpace.rect.width / 2)
-            direction = Vector3.right;
+        if (gameObject.transform.localPosition.x < _space.rect.center.x - _space.rect.width / 2)
+            _direction = Vector3.right;
 
-        if (gameObject.transform.localPosition.x > mSpace.rect.center.x + mSpace.rect.width / 2)
-            direction = Vector3.left;
+        if (gameObject.transform.localPosition.x > _space.rect.center.x + _space.rect.width / 2)
+            _direction = Vector3.left;
 
-        if (gameObject.transform.localPosition.y < mSpace.rect.center.y - mSpace.rect.height / 2)
-            direction = Vector3.up;
+        if (gameObject.transform.localPosition.y < _space.rect.center.y - _space.rect.height / 2)
+            _direction = Vector3.up;
 
-        if (gameObject.transform.localPosition.y > mSpace.rect.center.y + mSpace.rect.height / 2)
-            direction = Vector3.down;
+        if (gameObject.transform.localPosition.y > _space.rect.center.y + _space.rect.height / 2)
+            _direction = Vector3.down;
 
-        gameObject.transform.Translate(direction * Time.deltaTime * speed);
+        gameObject.transform.Translate(_direction * Time.deltaTime * _speed);
     }
     #endregion
 
     #region methods of how person infect others
     void CheckInfection()
     {
-        if (mStatus == E_Person_Status.Infected || mStatus == E_Person_Status.Incubation)
+        if (_status == E_Person_Status.Infected || _status == E_Person_Status.Incubation)
         {
-            currentInfectionFrame++;
-            if (currentInfectionFrame > checkInfectionFrame && mCity != null)
+            _currentInfectionFrame ++;
+            if (_currentInfectionFrame > _checkInfectionFrame && _city != null)
             {
                 StartCoroutine(CheckInfectionCoroutine());
+                _currentInfectionFrame = 0;
             }
         }
     }
@@ -215,19 +216,20 @@ public class Person : MonoBehaviour
     IEnumerator CheckInfectionCoroutine()
     {
         float infectionRate = Virus.infectionRate * Virus.intentionToMove;
-        for(int i = 0; mCity!= null && i < mCity.people.Count; i++)
+        Debug.Log(infectionRate);
+        for(int i = 0; _city != null && i < _city.people.Count; i++)
         {
-            if (mCity.people[i].mStatus == E_Person_Status.Healthy && Vector3.Distance(gameObject.transform.localPosition, mCity.people[i].gameObject.transform.localPosition) < 20f && Random.Range(0, 1) < infectionRate)
+            if (_city.people[i]._status == E_Person_Status.Healthy && Vector3.Distance(gameObject.transform.localPosition, _city.people[i].gameObject.transform.localPosition) < 20f && Random.Range(0f, 1f) < infectionRate)
             {
-                EventCenter.GetInstance().EventTrigger<E_Person_Status>("Minus Data", mCity.people[i].mStatus);
+                EventCenter.GetInstance().EventTrigger<E_Person_Status>("Minus Data", _city.people[i]._status);
                 if (Virus.incubationPeriod > 0)
-                    mCity.people[i].ChangeStatus(E_Person_Status.Incubation);
+                    _city.people[i].ChangeStatus(E_Person_Status.Incubation);
                
                 else
-                    mCity.people[i].ChangeStatus(E_Person_Status.Infected);
+                    _city.people[i].ChangeStatus(E_Person_Status.Infected);
         
                     
-                EventCenter.GetInstance().EventTrigger<E_Person_Status>("Add Data", mCity.people[i].mStatus);
+                EventCenter.GetInstance().EventTrigger<E_Person_Status>("Add Data", _city.people[i]._status);
             }
 
             if (i % 100 == 0)
@@ -243,11 +245,11 @@ public class Person : MonoBehaviour
     /// </summary>
     void CheckHospital()
     {
-        if (mStatus == E_Person_Status.Infected)
+        if (_status == E_Person_Status.Infected)
         {
-            waitTimeToHospital += Time.deltaTime;
+            _waitTimeToHospital += Time.deltaTime;
 
-            if (waitTimeToHospital > Virus.responseTime)
+            if (_waitTimeToHospital > Virus.responseTime)
                 EventCenter.GetInstance().EventTrigger<Person>("Add Patient", this);
         }
     }
@@ -259,10 +261,10 @@ public class Person : MonoBehaviour
     /// </summary>
     void IncubationToInfected()
     {
-        if (mStatus == E_Person_Status.Incubation)
+        if (_status == E_Person_Status.Incubation)
         {
-            waitTimeToInfected += Time.deltaTime;
-            if (waitTimeToInfected > Virus.incubationPeriod)
+            _waitTimeToInfected += Time.deltaTime;
+            if (_waitTimeToInfected > Virus.incubationPeriod)
             {
                 EventCenter.GetInstance().EventTrigger<Person.E_Person_Status>("Minus Data", Person.E_Person_Status.Incubation);
                 EventCenter.GetInstance().EventTrigger<Person.E_Person_Status>("Add Data", Person.E_Person_Status.Infected);
@@ -272,15 +274,18 @@ public class Person : MonoBehaviour
     }
     #endregion
 
+
+
     #region Methods of monobehaviors
     void Update()
     {
-        if (gameStart)
+        if (_gameStart)
         {
             RandomMove();
             CheckInfection();
             CheckHospital();
             IncubationToInfected();
+            //Cure();
         }
     }
     #endregion
